4b49345d5c4d6017b03a87a5855dadd2d4ab4164
zhangmeng <zhangmeng@forwardx.com>
2018-10-31 14:31:30 +0800

    An example show how to config you project with cmake

---
+++ src/math/math_function.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+double power(double base, int exponent){
+	int result = base;
+	if(exponent == 0){
+		return 1;
+	}
+	for (int i = 1; i < exponent; ++i){
+		result = result * base;
+	}
+	return result;
+}
